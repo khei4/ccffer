47db13e10aa48e536914a31e1965f65df53d5ae4
khei4 <kk.asano.luxy@gmail.com>
2022-03-17 15:25:03 +0900

    Make table's key types.Type

--- typetable.go
+++ typetable.go
@@ -2,12 +2,16 @@
 // https://stackoverflow.com/questions/6878590/the-maximum-value-for-an-int-type-in-go
 package main
 
-var TypeVals map[string][]string = map[string][]string{
-	"bool":   {"true", "false"},
-	"string": {""},
-	"int":    {"0", "-(1 << 31) - 1", "1 << 31"},
-	"int64":  {"0", "-(1 << 63) - 1", "1 << 63"},
-	"uint":   {"0", "^uint(0)"},
+import "go/types"
+
+// 値でもったほうがいい?
+// Pointer, Interfaceは実際に取ってきてから作らないと厳しそう
+var TypeVals map[types.Type][]string = map[types.Type][]string{
+	types.Typ[types.Bool]:   {"true", "false"},
+	types.Typ[types.String]: {""},
+	types.Typ[types.Int]:    {"0", "-2147483648", "2147483647"},
+	types.Typ[types.Int64]:  {"0", "-9223372036854775808", "9223372036854775807"},
+	types.Typ[types.Uint]:   {"0", "18446744073709551615"},
 	// TODO: import math library by formatting test
 	// "float32": []string{"0", "math.NaN()", "math.Inf(0)", "math.Inf(-1)"},
 }
