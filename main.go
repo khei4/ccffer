a1dafe51cac0d56991eaf2a93f9820621bacc966
khei4 <kk.asano.luxy@gmail.com>
2022-03-18 19:14:55 +0900

    exit with code 1 if error happens

--- main.go
+++ main.go
@@ -17,12 +17,12 @@ func main() {
 	templData, err := info.GetTemplDataFromPackages(os.Args[1:])
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "cannot open %s", os.Args[1:])
-		return
+		os.Exit(1)
 	}
 	test, err := testgen.GenTests(templData)
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "unexpected error: %s", err)
-		return
+		os.Exit(1)
 	}
 	fp, err := os.Create(os.Args[1] + "/" + os.Args[1] + "_test.go")
 	fp.WriteString(test)
